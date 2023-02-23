ca4008312b38a642af7f15bb26190e440bc3d1e2
a-henrique <audrinh@gmail.com>
2023-02-23 13:07:04 -0300

    first commit

---
+++ src/main/java/com/devsuperior/userdpt/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.userdpt.repositories;
+
+import com.devsuperior.userdpt.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
